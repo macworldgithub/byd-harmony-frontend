766eaf142ee3058466125da6a184a50c6a0aa332
Ahmed <m.ahmed.fahim02@gmail.com>
2026-08-10 11:05:19 +0500

    create structure

--- src/components/layout/DashboardShell.tsx
+++ src/components/layout/DashboardShell.tsx
@@ -1,5 +1,4 @@
 import { Sidebar } from "./Sidebar";
-import { Footer } from "./Footer";
 import type { RoleConfig } from "@/lib/types";
 
 export function DashboardShell({
@@ -14,7 +13,6 @@ export function DashboardShell({
       <Sidebar role={role} />
       <div className="flex flex-1 flex-col min-w-0">
         <main className="flex-1 overflow-x-hidden px-5 py-6 sm:px-8 sm:py-8">{children}</main>
-        {/* <Footer compact /> */}
       </div>
     </div>
   );
